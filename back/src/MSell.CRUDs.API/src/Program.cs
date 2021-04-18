5a793c8b092908229aa38689c95389d410977fc3
Carlos Mario <oliveiracarlosmario1@gmail.com>
2021-04-18 17:09:27 -0300

    Fixed Program.cs

--- back/src/MSell.CRUDs.API/src/Program.cs
+++ back/src/MSell.CRUDs.API/src/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using MSell.CRUDs.API.src;
 
-namespace MSell.CRUDs.API
+namespace MSell.CRUDs.API.src
 {
     public class Program
     {
